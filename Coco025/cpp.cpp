624fb73f31db996233e93d8815041ed13cb434ab
Coco025! <luiscocose025@gmail.com>
2019-05-01 11:28:08 -0600

    .

--- Coco025/cpp.cpp
+++ Coco025/cpp.cpp
@@ -6,8 +6,9 @@ typedef long long ll;
 typedef pair<int, int> ii;
 int casos;
 
+int a;
 int main(){
     //cin.tie(0),ios_base::sync_with_stdio(0),cout.tie(0);
-    
+    cin >> a;
     return 0;
 }
